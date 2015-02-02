dc54986d214c1d26e048cffb7c390daa860443d5
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-02-02 06:06:21 -0500

    Se añaden nuevas funcionalidades los views, siguiendo el instructivo del curso, y se crea el archivo .gitignore

---
+++ userprofiles/urls.py
@@ -0,0 +1,6 @@
+from django. conf.urls import patterns, urls
+from userprofiles.views import LoginView
+
+urlpatterns = patterns('',
+	url(r'^login/$', LoginView.as_view(), name='login'),
+)
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,19 +1,28 @@
+from django.views.generic import View
+from django.http import HttpResponse
+
 from django.shortcuts import render
 from .forms import UserCreationEmailForm, EmailAuthenticationForm
 from django.contrib.auth import login
 
-def signup(request):
-	form = UserCreationEmailForm(request.POST or None)
+class LoginView(View):
+	
+	def get(self, request, *args, **kwargs):
+		return HttpResponse('LoginView!!')
 
-	if form.is_valid():
-		form.save()
 
-	return render(request, 'signup.html', {'form': form})
+# def signup(request):
+# 	form = UserCreationEmailForm(request.POST or None)
 
-def signin(request):
-	form = EmailAuthenticationForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
 
-	if form.is_valid():
-		login(request, form.get_user())
+# 	return render(request, 'signup.html', {'form': form})
 
-	return render(request, 'signin.html', {'form': form})
+# def signin(request):
+# 	form = EmailAuthenticationForm(request.POST or None)
+
+# 	if form.is_valid():
+# 		login(request, form.get_user())
+
+# 	return render(request, 'signin.html', {'form': form})
