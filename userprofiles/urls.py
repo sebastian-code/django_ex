5225f4ea9365048f920968c3a3ef2330986ebdda
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-02-03 09:51:47 -0500

    Se hacen actualizaciones al proceso de registro al administrador

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -3,4 +3,5 @@ from userprofiles.views import LoginView
 
 urlpatterns = patterns('',
 	url(r'^login/$', LoginView.as_view(), name='login'),
+	
 )
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -5,24 +5,24 @@ from django.shortcuts import render
 from .forms import UserCreationEmailForm, EmailAuthenticationForm
 from django.contrib.auth import login
 
-class LoginView(View):
+# class LoginView(View):
 	
-	def get(self, request, *args, **kwargs):
-		return HttpResponse('LoginView!!')
+# 	def get(self, request, *args, **kwargs):
+# 		return HttpResponse('LoginView!!')
 
 
-# def signup(request):
-# 	form = UserCreationEmailForm(request.POST or None)
+def signup(request):
+	form = UserCreationEmailForm(request.POST or None)
 
-# 	if form.is_valid():
-# 		form.save()
+	if form.is_valid():
+		form.save()
 
-# 	return render(request, 'signup.html', {'form': form})
+	return render(request, 'signup.html', {'form': form})
 
-# def signin(request):
-# 	form = EmailAuthenticationForm(request.POST or None)
+def signin(request):
+	form = EmailAuthenticationForm(request.POST or None)
 
-# 	if form.is_valid():
-# 		login(request, form.get_user())
+	if form.is_valid():
+		login(request, form.get_user())
 
-# 	return render(request, 'signin.html', {'form': form})
+	return render(request, 'signin.html', {'form': form})
