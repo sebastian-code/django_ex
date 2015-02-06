7c49b0c610c9800423c3544d8701fe20a31448aa
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-02-06 11:46:11 -0500

    Se modifica visualizacion de los tracks para incluir titulo en la plantilla

--- src/urls.py
+++ src/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^tracks/(?P<title>[\w\-]+)/', 'tracks.views.track_view', name='track_view'),
+    url(r'^tracks/(?P<title>[\w\-\W]+)/', 'tracks.views.track_view', name='track_view'),
     url(r'^signup/', 'userprofiles.views.signup', name='signup'),
     url(r'^signin/', 'userprofiles.views.signin', name='signin'),
 )
--- tracks/models.py
+++ tracks/models.py
@@ -4,12 +4,14 @@ from albums.models import Album
 from artists.models import Artist
 
 class Track(models.Model):
-	"""docstring for Track"""
 	title = models.CharField(max_length=255)
 	order = models.PositiveIntegerField()
 	track_file = models.FileField(upload_to="tracks")
 	album = models.ForeignKey(Album)
 	artist = models.ForeignKey(Artist)
+
+	def get_absolut_urk(self):
+		return '/tracks/' + self.title
 
 	def player(self):
 		return """
